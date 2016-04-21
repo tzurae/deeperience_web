ee51b5d63c8496e05227f0641613c0b3cade1a70
gocreating <gocreating@gmail.com>
2016-04-21 12:28:47 +0800

    Clean code

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 import express from 'express';
-// import mongoose from 'mongoose';
-// import mongolab from './credentials/mongolab';
 import middlewares from './middlewares';
 import routes from './routes';
 
