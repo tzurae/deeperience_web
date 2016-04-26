ed6ec65ac0076c075aa55475f5105a2147973105
gocreating <gocreating@gmail.com>
2016-04-26 23:44:26 +0800

    Implement authorized request api

--- src/flux/utils/webAPIUtils.js
+++ src/flux/utils/webAPIUtils.js
@@ -1,4 +1,10 @@
 import 'whatwg-fetch';
+import getToken from './getToken';
+
+const getUnAuthPromise = () => new Promise((resolve, reject) => {
+  reject(new Error(
+    'Calling authenticated required api without providing token'));
+});
 
 export default {
   get: (path) => {
@@ -16,6 +22,35 @@ export default {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(param),
+    }).then((res) => res.json());
+  },
+  getAuth: (path) => {
+    const token = getToken();
+    if (!token) {
+      return getUnAuthPromise();
+    }
+    return fetch(path, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    }).then((res) => res.json());
+  },
+  postAuth: (path, param = {}) => {
+    const token = getToken();
+    if (!token) {
+      return getUnAuthPromise();
+    }
+    return fetch(path, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: token,
       },
       body: JSON.stringify(param),
     }).then((res) => res.json());
