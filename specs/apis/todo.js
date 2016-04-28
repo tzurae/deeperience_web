a7c0fcbcd4202f496350884a3100c47c53b6084e
gocreating <gocreating@gmail.com>
2016-04-28 14:54:47 +0800

    Refactor tests

--- specs/apis/todo.js
+++ specs/apis/todo.js
@@ -1,37 +1,63 @@
 var chai = require('chai');
 var request = require('superagent');
 var expect = chai.expect;
+var async = require('async');
 var constants = require('../constants');
 var Todo = require('../../build/server/models/Todo').default;
 
-var fakeTodo = {
-  text: 'this is a fake todo text',
-};
-var resTodo;
+describe('#todo', function() {
+  var fakeTodos = [{
+    text: 'this is a fake todo text',
+  }, {
+    text: 'foo',
+  }, {
+    text: '~bar~',
+  }, ];
+  var resTodos = [];
 
-before(function(done) {
-  Todo.remove({}, done);
-});
+  before(function(done) {
+    Todo.remove({}, done);
+  });
 
-describe('#Unauthorized User', function() {
-  // POST /api/todo
-  describe('GET /api/todo', function() {
-    it('should create todo', function(done) {
-      request
-        .post(constants.BASE + '/api/todo')
-        .send(fakeTodo)
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          expect(res.body.todo).to.be.an('object');
-          resTodo = res.body.todo;
-          done();
-        });
+  describe('#Unauthorized User', function() {
+    // POST /api/todo
+    describe('POST /api/todo', function() {
+      it('should create todo', function(done) {
+        async.eachSeries(fakeTodos, function iteratee(fakeTodo, cb) {
+          request
+            .post(constants.BASE + '/api/todo')
+            .send(fakeTodo)
+            .end(function(err, res) {
+              expect(res).to.not.be.undefined;
+              expect(res.status).to.equal(200);
+              expect(res.body.isError).to.be.false;
+              expect(res.body.todo).to.be.an('object');
+              expect(res.body.todo.text).to.equal(fakeTodo.text);
+              resTodos.push(res.body.todo);
+              cb();
+            });
+        }, done);
+      });
+    });
+
+    // GET /api/todo
+    describe('GET /api/todo', function() {
+      it('should list todos', function(done) {
+        request
+          .get(constants.BASE + '/api/todo')
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            expect(res.body.isError).to.be.false;
+            expect(res.body.todos).to.be.an('array');
+            expect(res.body.todos.length).to.equal(fakeTodos.length);
+            done();
+          });
+      });
     });
   });
-});
 
-after(function(done) {
-  Todo.remove({}, done);
+  after(function(done) {
+    Todo.remove({}, done);
+  });
 });
--- specs/apis/user.js
+++ specs/apis/user.js
@@ -4,129 +4,131 @@ var expect = chai.expect;
 var constants = require('../constants');
 var User = require('../../build/server/models/User').default;
 
-var fakeUser = {
-  email: 'fakeuser@gmail.com',
-  password: 'fake',
-};
-var resUser;
+describe('#user', function() {
+  var fakeUser = {
+    email: 'fakeuser@gmail.com',
+    password: 'fake',
+  };
+  var resUser;
 
-var validateUser = function(user) {
-  expect(user).to.contain.all.keys(['_id', 'email']);
-  expect(user).to.not.have.any.keys(['password']);
-};
+  var validateUser = function(user) {
+    expect(user).to.contain.all.keys(['_id', 'email']);
+    expect(user).to.not.have.any.keys(['password']);
+  };
 
-before(function(done) {
-  User.remove({}, done);
-});
-
-describe('#Unauthorized User', function() {
-  // POST /api/user
-  describe('POST /api/user', function() {
-    it('should create user', function(done) {
-      request
-        .post(constants.BASE + '/api/user')
-        .send(fakeUser)
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          validateUser(res.body.user);
-          resUser = res.body.user;
-          done();
-        });
-    });
+  before(function(done) {
+    User.remove({}, done);
   });
 
-  // POST /api/user/login
-  describe('POST /api/user/login', function() {
-    it('should auth valid user', function(done) {
-      request
-        .post(constants.BASE + '/api/user/login')
-        .send(fakeUser)
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          expect(res.body.isAuth).to.be.true;
-          expect(res.body.token).to.be.a('string');
-          done();
-        });
-    });
-    it('should reject invalid user', function(done) {
-      request
-        .post(constants.BASE + '/api/user/login')
-        .send({})
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          expect(res.body.isAuth).to.be.false;
-          done();
-        });
-    });
-  });
-
-  // GET /api/user/logout
-  describe('GET /api/user/logout', function() {
-    it('should unauth user', function(done) {
-      request
-        .get(constants.BASE + '/api/user/logout')
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          done();
-        });
-    });
-    it('should reject invalid user', function(done) {
-      request
-        .post(constants.BASE + '/api/user/login')
-        .send({})
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          expect(res.body.isError).to.be.false;
-          expect(res.body.isAuth).to.be.false;
-          done();
-        });
-    });
-  });
-
-  // GET /api/user/me
-  describe('GET /api/user/me', function() {
-    it('should be rejected', function(done) {
-      request
-        .get(constants.BASE + '/api/user/me')
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(401);
-          done();
-        });
-    });
-  });
-});
-
-describe('#Authorized User', function() {
-  // GET /api/user/me
-  describe('GET /api/user/me', function() {
-    it('should show user', function(done) {
-      User.findOne({}, (err, user) => {
-        var token = user.toJwtToken();
+  describe('#Unauthorized User', function() {
+    // POST /api/user
+    describe('POST /api/user', function() {
+      it('should create user', function(done) {
         request
-          .get(constants.BASE + '/api/user/me')
-          .set('Authorization', 'JWT ' + token)
+          .post(constants.BASE + '/api/user')
+          .send(fakeUser)
           .end(function(err, res) {
             expect(res).to.not.be.undefined;
             expect(res.status).to.equal(200);
             expect(res.body.isError).to.be.false;
             validateUser(res.body.user);
+            resUser = res.body.user;
+            done();
+          });
+      });
+    });
+
+    // POST /api/user/login
+    describe('POST /api/user/login', function() {
+      it('should auth valid user', function(done) {
+        request
+          .post(constants.BASE + '/api/user/login')
+          .send(fakeUser)
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            expect(res.body.isError).to.be.false;
+            expect(res.body.isAuth).to.be.true;
+            expect(res.body.token).to.be.a('string');
+            done();
+          });
+      });
+      it('should reject invalid user', function(done) {
+        request
+          .post(constants.BASE + '/api/user/login')
+          .send({})
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            expect(res.body.isError).to.be.false;
+            expect(res.body.isAuth).to.be.false;
+            done();
+          });
+      });
+    });
+
+    // GET /api/user/logout
+    describe('GET /api/user/logout', function() {
+      it('should unauth user', function(done) {
+        request
+          .get(constants.BASE + '/api/user/logout')
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            expect(res.body.isError).to.be.false;
+            done();
+          });
+      });
+      it('should reject invalid user', function(done) {
+        request
+          .post(constants.BASE + '/api/user/login')
+          .send({})
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            expect(res.body.isError).to.be.false;
+            expect(res.body.isAuth).to.be.false;
+            done();
+          });
+      });
+    });
+
+    // GET /api/user/me
+    describe('GET /api/user/me', function() {
+      it('should be rejected', function(done) {
+        request
+          .get(constants.BASE + '/api/user/me')
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(401);
             done();
           });
       });
     });
   });
-});
 
-after(function(done) {
-  User.remove({}, done);
+  describe('#Authorized User', function() {
+    // GET /api/user/me
+    describe('GET /api/user/me', function() {
+      it('should show user', function(done) {
+        User.findOne({}, (err, user) => {
+          var token = user.toJwtToken();
+          request
+            .get(constants.BASE + '/api/user/me')
+            .set('Authorization', 'JWT ' + token)
+            .end(function(err, res) {
+              expect(res).to.not.be.undefined;
+              expect(res.status).to.equal(200);
+              expect(res.body.isError).to.be.false;
+              validateUser(res.body.user);
+              done();
+            });
+        });
+      });
+    });
+  });
+
+  after(function(done) {
+    User.remove({}, done);
+  });
 });
