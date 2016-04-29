f246de523ba51bd6c37edd12525201207fc7e928
gocreating <gocreating@gmail.com>
2016-04-29 14:35:49 +0800

    Customize passport 401 handler

--- src/server/middlewares/authRequired.js
+++ src/server/middlewares/authRequired.js
@@ -1,7 +1,20 @@
 import passport from 'passport';
 
-const authRequired = passport.authenticate('jwt', {
-  session: false,
-});
+const authRequired = (req, res, next) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // custom 401 message
+      return res.status(401).json({
+        isError: true,
+        info,
+      });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 export default authRequired;
