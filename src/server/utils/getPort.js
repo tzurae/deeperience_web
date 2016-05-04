04edb0a457ed0bc3d83cce6a5cfd37b869ba273c
gocreating <gocreating@gmail.com>
2016-05-04 15:04:11 +0800

    Improve getPort
    `process.argv[2]` coud be a string that is not a port number, which may
    leads to bugs.
    
    For example, in the case of testing, we run `mocha specs/index.js
    --timeout 10000`, and that makes `process.argv[2]` a string
    `specs/index.js` instrad of a valid port number

--- src/server/utils/getPort.js
+++ src/server/utils/getPort.js
@@ -1,1 +1,9 @@
-export default () => process.env.PORT || process.argv[2] || 3000;
+export default () => {
+  const args = process.argv.slice(2);
+  const portIndex = args.indexOf('-p');
+  let argPort = null;
+  if (portIndex >= 0) {
+    argPort = parseInt(args[portIndex + 1]);
+  }
+  return process.env.PORT || argPort || 3000;
+};
