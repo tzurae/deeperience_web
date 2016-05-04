bf12730c1c9435a0f0edc663745ee578fbe38f88
gocreating <gocreating@gmail.com>
2016-05-04 15:21:07 +0800

    Remove redundant argument

--- src/flux/api/todo.js
+++ src/flux/api/todo.js
@@ -1,7 +1,7 @@
 import fetch from '../utils/webAPIUtils';
 
 export default {
-  list: (todo) => fetch.get('/api/todo'),
+  list: () => fetch.get('/api/todo'),
   create: (todo) => fetch.post('/api/todo', todo),
   remove: (id) => fetch.delete(`/api/todo/${id}`),
 };
