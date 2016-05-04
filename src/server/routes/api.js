b01d369502a1d24e48bfcd2c1de044204cb0f9f7
gocreating <gocreating@gmail.com>
2016-05-04 16:00:21 +0800

    Refactor server side routing

---
+++ src/server/routes/api.js
@@ -0,0 +1,13 @@
+import authRequired from '../middlewares/authRequired';
+import userController from '../controllers/user';
+import todoController from '../controllers/todo';
+
+export default ({ app }) => {
+  app.post('/api/user', userController.create);
+  app.post('/api/user/login', userController.login);
+  app.get('/api/user/logout', userController.logout);
+  app.get('/api/user/me', authRequired, userController.show);
+  app.post('/api/todo', todoController.create);
+  app.get('/api/todo', todoController.list);
+  app.delete('/api/todo/:id', todoController.remove);
+};
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,15 +1,7 @@
-import authRequired from '../middlewares/authRequired';
-import userController from '../controllers/user';
-import todoController from '../controllers/todo';
-import reactController from '../controllers/react';
+import apiRoutes from './api';
+import ssrRoutes from './ssr';
 
 export default ({ app }) => {
-  app.post('/api/user', userController.create);
-  app.post('/api/user/login', userController.login);
-  app.get('/api/user/logout', userController.logout);
-  app.get('/api/user/me', authRequired, userController.show);
-  app.post('/api/todo', todoController.create);
-  app.get('/api/todo', todoController.list);
-  app.delete('/api/todo/:id', todoController.remove);
-  app.get('/*', reactController.render);
+  apiRoutes({ app });
+  ssrRoutes({ app });
 };
