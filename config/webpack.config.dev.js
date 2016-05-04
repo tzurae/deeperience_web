bd90753523e1c76fff9207e79717884b82aeb980
gocreating <gocreating@gmail.com>
2016-05-04 14:46:09 +0800

    Fix bug
    Patch the missed essential configuration for webAPIUtils

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -23,6 +23,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
+        BROWSER: JSON.stringify(true),
       },
     }),
     new webpack.NoErrorsPlugin(),
