1e7584771848e48910846b0313b7afd9e3878159
gocreating <gocreating@gmail.com>
2016-05-05 01:58:05 +0800

    Add comment for simulating timeout

--- src/flux/api/todo.js
+++ src/flux/api/todo.js
@@ -1,6 +1,12 @@
 import api from '../utils/webAPIUtils';
 
 export default {
+  // below is an example of simulating timeout for `ssr fetch state`
+  // list: () => new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve(api.get('/api/todo'));
+  //   }, 5000);
+  // }),
   list: () => api.get('/api/todo'),
   create: (todo) => api.post('/api/todo', todo),
   remove: (id) => api.delete(`/api/todo/${id}`),
