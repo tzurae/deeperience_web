37962b39ef2302a71c1ebdce0f488ccc6cf06fb3
gocreating <gocreating@gmail.com>
2016-05-04 21:32:44 +0800

    Add state fetching for user

--- src/server/controllers/ssrFetchState.js
+++ src/server/controllers/ssrFetchState.js
@@ -2,6 +2,15 @@ import todoAPI from '../../flux/api/todo';
 import wrapTimeout from '../decorators/wrapTimeout';
 
 export default {
+  user: (req, res, next) => {
+    res.setSSRState({
+      user: {
+        token: req.cookies['token'],
+        data: req.user,
+      },
+    });
+    next();
+  },
   todo: wrapTimeout(3000)((req, res, next) => {
     todoAPI
       .list()
--- src/server/routes/ssrFetchState.js
+++ src/server/routes/ssrFetchState.js
@@ -1,5 +1,7 @@
+import authRequired from '../middlewares/authRequired';
 import ssrFetchStateController from '../controllers/ssrFetchState';
 
 export default ({ app }) => {
+  app.use('/user/me', authRequired, ssrFetchStateController.user);
   app.get('/todo', ssrFetchStateController.todo);
 };
