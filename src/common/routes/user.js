3bb63ab723083515b2fa34c70cb36e30d6023429
gocreating <gocreating@gmail.com>
2016-05-10 11:09:48 +0800

    Implement lazy load for user routes

--- src/common/routes/user.js
+++ src/common/routes/user.js
@@ -1,23 +1,21 @@
-import authRequired from '../utils/authRequired';
-import RegisterPage from '../views/pages/user/RegisterPage';
-import LoginPage from '../views/pages/user/LoginPage';
-import LogoutPage from '../views/pages/user/LogoutPage';
-import ShowPage from '../views/pages/user/ShowPage';
-
 export default {
   path: 'user',
-  childRoutes: [{
-    path: 'register',
-    component: RegisterPage,
-  }, {
-    path: 'login',
-    component: LoginPage,
-  }, {
-    path: 'logout',
-    component: LogoutPage,
-  }, {
-    path: 'me',
-    component: ShowPage,
-    onEnter: authRequired,
-  }, ],
+  getChildRoutes(location, cb) {
+    require.ensure([], (require) => {
+      cb(null, [{
+        path: 'register',
+        component: require('../views/pages/user/RegisterPage').default,
+      }, {
+        path: 'login',
+        component: require('../views/pages/user/LoginPage').default,
+      }, {
+        path: 'logout',
+        component: require('../views/pages/user/LogoutPage').default,
+      }, {
+        path: 'me',
+        component: require('../views/pages/user/ShowPage').default,
+        onEnter: require('../utils/authRequired').default,
+      }, ]);
+    });
+  },
 };
