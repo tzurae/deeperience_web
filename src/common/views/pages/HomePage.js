9487ad91a3c491e21c6e99c9585b3ea1ac4dac7e
gocreating <gocreating@gmail.com>
2016-05-15 01:51:51 +0800

    Refactor components
    Rewrite class-based components into functional components

--- src/common/views/pages/HomePage.js
+++ src/common/views/pages/HomePage.js
@@ -2,20 +2,18 @@ import React from 'react';
 import PageLayout from '../layouts/PageLayout';
 import BsPageHeader from '../components/BsPageHeader';
 
-export default class HomePage extends React.Component {
-  render() {
-    return (
-      <PageLayout>
-        <BsPageHeader title="Express-React-HMR-Boilerplate" />
-        <p>
-          This is the demo site for project
-          <a
-            href="https://github.com/gocreating/express-react-hmr-boilerplate">
-            <span> </span>
-            express-react-hmr-boilerplate
-          </a>
-        </p>
-      </PageLayout>
-    );
-  }
-};
+const HomePage = (props) => (
+  <PageLayout>
+    <BsPageHeader title="Express-React-HMR-Boilerplate" />
+    <p>
+      This is the demo site for project
+      <a
+        href="https://github.com/gocreating/express-react-hmr-boilerplate">
+        <span> </span>
+        express-react-hmr-boilerplate
+      </a>
+    </p>
+  </PageLayout>
+);
+
+export default HomePage;
--- src/common/views/pages/NotFoundPage.js
+++ src/common/views/pages/NotFoundPage.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PageLayout from '../layouts/PageLayout';
 import BsPageHeader from '../components/BsPageHeader';
 
-export default class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <PageLayout>
-        <BsPageHeader title="Page Not Found" />
-        <p>This is a 404 page.</p>
-      </PageLayout>
-    );
-  }
-};
+const NotFoundPage = (props) => (
+  <PageLayout>
+    <BsPageHeader title="Page Not Found" />
+    <p>This is a 404 page.</p>
+  </PageLayout>
+);
+
+export default NotFoundPage;
