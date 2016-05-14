b4dec5a07e9f728ed2cc9787eede39b2f771e7ed
gocreating <gocreating@gmail.com>
2016-05-15 00:24:19 +0800

    Add server side Locale#show api

---
+++ src/server/controllers/locale.js
@@ -0,0 +1,24 @@
+export default {
+  show(req, res) {
+    try {
+      // escape file path for security
+      const locale = req.params.locale.replace(/\./g, '').replace(/\//g, '');
+      const messages = require(`../../common/i18n/${locale}`).default;
+
+      res.json({
+        locale,
+        messages,
+        isError: false,
+      });
+    } catch (e) {
+      res.json({
+        errors: [{
+          name: 'Locale is not supported',
+          message: 'Locale is not supported',
+        },],
+        status: 400,
+        isError: true,
+      });
+    }
+  },
+};
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,5 +1,6 @@
 import authRequired from '../middlewares/authRequired';
 import userController from '../controllers/user';
+import localeController from '../controllers/locale';
 import todoController from '../controllers/todo';
 
 export default ({ app }) => {
@@ -7,6 +8,7 @@ export default ({ app }) => {
   app.post('/api/user/login', userController.login);
   app.get('/api/user/logout', userController.logout);
   app.get('/api/user/me', authRequired, userController.show);
+  app.get('/api/locale/:locale', localeController.show);
   app.post('/api/todo', todoController.create);
   app.get('/api/todo', todoController.list);
   app.delete('/api/todo/:id', todoController.remove);
