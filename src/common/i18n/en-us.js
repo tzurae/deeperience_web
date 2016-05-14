ec41105b869ea2ea6bb2180dc6e6725305ef49e7
gocreating <gocreating@gmail.com>
2016-05-15 00:12:32 +0800

    Add locale data

---
+++ src/common/i18n/en-us.js
@@ -0,0 +1,11 @@
+export default {
+  parentLocale: 'en',
+  'nav.home': 'Home',
+  'nav.todo': 'Todo',
+  'nav.language': 'Language',
+  'nav.user': 'User',
+  'nav.user.login': 'Login',
+  'nav.user.register': 'Register',
+  'nav.user.profile': 'Profile',
+  'nav.user.logout': 'Logout',
+};
---
+++ src/common/i18n/zh-tw.js
@@ -0,0 +1,11 @@
+export default {
+  parentLocale: 'zh',
+  'nav.home': '首頁',
+  'nav.todo': '待辦',
+  'nav.language': '語言',
+  'nav.user': '會員',
+  'nav.user.login': '登入',
+  'nav.user.register': '註冊',
+  'nav.user.profile': '會員中心',
+  'nav.user.logout': '登出',
+};
