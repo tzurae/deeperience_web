c22d71373a4eea2f3a32ebb346e62017a802f664
gocreating <gocreating@gmail.com>
2016-05-23 02:29:41 +0800

    Add server side Firebase#readToken api

---
+++ src/server/controllers/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+export default {
+  readToken(req, res) {
+    // ref: <https://firebase.google.com/docs/auth/server#create_a_custom_token>
+    let token = firebase.auth().createCustomToken(req.user._id.toString());
+    res.json({
+      token: token,
+      isError: false,
+    });
+  },
+};
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -2,6 +2,7 @@ import bodyParser from '../middlewares/bodyParser';
 import authRequired from '../middlewares/authRequired';
 import fileUpload from '../middlewares/fileUpload';
 import userController from '../controllers/user';
+import firebaseController from '../controllers/firebase';
 import localeController from '../controllers/locale';
 import todoController from '../controllers/todo';
 
@@ -10,6 +11,8 @@ export default ({ app }) => {
   app.post('/api/user/login', bodyParser.json, userController.login);
   app.get('/api/user/logout', userController.logout);
   app.get('/api/user/me', authRequired, userController.show);
+  app.get('/api/user/me/firebase/token',
+    authRequired, firebaseController.readToken);
   app.post('/api/user/me/avatar',
     authRequired,
     fileUpload.memory.single('avatar'),
