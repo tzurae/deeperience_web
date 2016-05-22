2bc21cc43966d0e6545b5a08327c4f26b838c64a
gocreating <gocreating@gmail.com>
2016-05-22 16:45:01 +0800

    Refactor apis
    Remove unused arguments

--- src/common/api/user.js
+++ src/common/api/user.js
@@ -3,7 +3,7 @@ import api from '../utils/webAPIUtils';
 export default {
   register: (user) => api.post('/api/user', user),
   login: (user) => api.post('/api/user/login', user),
-  logout: (user) => api.get('/api/user/logout'),
-  show: (user) => api.getAuth('/api/user/me'),
+  logout: () => api.get('/api/user/logout'),
+  show: () => api.getAuth('/api/user/me'),
   uploadAvatar: (avatar) => api.postFormAuth('/api/user/me/avatar', avatar),
 };
