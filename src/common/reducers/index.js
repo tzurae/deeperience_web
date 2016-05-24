b9c55cc085d0f9fba0ff33c37dfdfbfebd00c15d
gocreating <gocreating@gmail.com>
2016-05-25 03:46:47 +0800

    Implement cookie store

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import cookie from './cookieReducer';
 import todos from './todoReducer';
 import user from './userReducer';
 import form from './formReducer';
 import intl from './intlReducer';
 
 const rootReducer = combineReducers({
+  cookie,
   todos,
   user,
   form,
