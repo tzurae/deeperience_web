43e48d04b65c7ad074754d7009f6ee6fe5596293
gocreating <gocreating@gmail.com>
2016-05-31 02:10:19 +0800

    Handle NProgress on route events

--- src/client/setupNProgress.js
+++ src/client/setupNProgress.js
@@ -1,4 +1,6 @@
-// ref: <https://gist.github.com/jgatjens/a08126bbbadb2d6096cb>
+// refs:
+// - <https://gist.github.com/jgatjens/a08126bbbadb2d6096cb>
+// - <http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom>
 export default () => {
   let oldOpen = XMLHttpRequest.prototype.open;
 
@@ -19,4 +21,43 @@ export default () => {
     this.addEventListener('readystatechange', onStateChange);
     oldOpen.apply(this, arguments);
   };
+
+  let observeDOM = (function() {
+    let MutationObserver = window.MutationObserver ||
+                           window.WebKitMutationObserver;
+    let eventListenerSupported = window.addEventListener;
+
+    return function(obj, onStart, onDone) {
+      let caller = function(ele) {
+        if (ele.nodeName === 'SCRIPT') {
+          onStart();
+          ele.onload = function() {
+            ele.onload = null;
+            onDone();
+          };
+        }
+      };
+      if (MutationObserver) {
+        let obs = new MutationObserver(function(mutations, observer) {
+          let ele = mutations[0].addedNodes.length &&
+                    mutations[0].addedNodes[0];
+          caller(ele);
+        });
+        obs.observe(obj, { childList: true, subtree: true });
+      } else if (eventListenerSupported) {
+        obj.addEventListener('DOMNodeInserted', function(e) {
+          caller(e.srcElement);
+        }, false);
+      }
+    };
+  })();
+
+  observeDOM(document.head,
+    function onStart() {
+      NProgress.start();
+    },
+    function onDone() {
+      NProgress.done();
+    }
+  );
 };
