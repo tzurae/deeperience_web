d4d7002ade9212e79eb3a4ead58ba5caf13c4ee8
翁治平 <gocreating@gmail.com>
2016-07-31 21:47:05 +0800

    Update tests

---
+++ specs/features/user.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  name: 'test1',
+  email: {
+    value: 'fakeUser1@gmail.com',
+  },
+  password: 'fake',
+}, {
+  name: 'test2',
+  email: {
+    value: 'fakeUser2@gmail.com',
+  },
+  password: 'fake123',
+},];
--- specs/pages.js
+++ specs/pages.js
@@ -2,18 +2,82 @@ var chai = require('chai');
 var request = require('superagent');
 var expect = chai.expect;
 var constants = require('./constants');
+var User = require('../build/server/models/User').default;
+var features = require('./features');
 
 describe('#Pages', function() {
-  describe('GET /', function() {
-    it('should respond a page', function(done) {
-      request
-        .get(constants.BASE + '/')
-        .end(function(err, res) {
-          expect(err).to.equal(null);
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          done();
+  var userTokens = [''];
+  var publicPages = [
+    '/',
+    '/user/register',
+    '/user/login',
+  ];
+  var privatePages = [
+    '/user/me',
+  ];
+
+  before(function(done) {
+    User(features.user[0]).save(function(err, user) {
+      userTokens[0] = user.toJwtToken();
+      done();
+    });
+  });
+
+  describe('#Unauthorized User', function() {
+    publicPages.forEach(function iteratee(page) {
+      describe('GET ' + page, function() {
+        it('should access a public page', function(cb) {
+          request
+            .get(constants.BASE + page)
+            .end(function(err, res) {
+              expect(err).to.equal(null);
+              expect(res).to.not.be.undefined;
+              expect(res.status).to.equal(200);
+              cb();
+            });
         });
+      });
+    });
+
+    privatePages.forEach(function iteratee(page) {
+      describe('GET ' + page, function() {
+        it('should redirect from a private page to login page', function(cb) {
+          request
+            .get(constants.BASE + page)
+            .end(function(err, res) {
+              expect(err).to.equal(null);
+              expect(res).to.not.be.undefined;
+              expect(res.status).to.equal(200);
+              expect(res.redirects).to.have.length.above(0);
+              expect(res.redirects[0]).to.have.string('/user/login');
+              cb();
+            });
+        });
+      });
+    });
+  });
+
+  describe('#Authorized User', function() {
+    (publicPages.concat(privatePages)).forEach(function iteratee(page) {
+      describe('GET ' + page, function() {
+        it('should access both public and private pages', function(cb) {
+          request
+            .get(constants.BASE + page)
+            .set('Cookie', 'token=' + userTokens[0])
+            .end(function(err, res) {
+              expect(err).to.equal(null);
+              expect(res).to.not.be.undefined;
+              expect(res.status).to.equal(200);
+              cb();
+            });
+        });
+      });
+    });
+  });
+
+  after(function(done) {
+    User.remove({email: features.user[0].email}, function(err) {
+      done();
     });
   });
 });
