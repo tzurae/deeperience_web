c654e942fc3d77aa0c986604d630d098e70c2343
翁治平 <gocreating@gmail.com>
2016-08-16 17:11:25 +0800

    Integrate with redux

--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Text, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
 import styles from '../styles';
 
-let Home = () => {
+let Home = ({ routes }) => {
   const goToLogin = () => Actions.login({text: 'Fuck you'});
 
   return (
     <View>
       <Text style={styles.bigblue}>
         Home
+      </Text>
+      <Text>
+        The current scene is titled {routes.scene.title}.
       </Text>
       <Text style={styles.red} onPress={goToLogin}>
         Go To About
@@ -18,4 +22,8 @@ let Home = () => {
   );
 };
 
-export default Home;
+Home.propTypes = {
+  routes: PropTypes.object,
+};
+
+export default connect(state => state)(Home);
--- src/native/index.js
+++ src/native/index.js
@@ -1,33 +1,4 @@
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import { Scene, Router } from 'react-native-router-flux';
-import Home from './components/Home';
-import About from './components/About';
-
-// ref: <https://github.com/aksonov/react-native-router-flux/blob/master/Example/Example.js>
-const getSceneStyle = (props, computedProps) => {
-  const style = {
-    flex: 1,
-    backgroundColor: '#fff',
-    shadowColor: null,
-    shadowOffset: null,
-    shadowOpacity: null,
-    shadowRadius: null,
-  };
-  if (computedProps.isActive) {
-    style.marginTop = computedProps.hideNavBar ? 0 : 64;
-    style.marginBottom = computedProps.hideTabBar ? 0 : 50;
-  }
-  return style;
-};
-
-let App = () => (
-  <Router getSceneStyle={getSceneStyle}>
-    <Scene key="root" hideNavBar={false}>
-      <Scene key="home" component={Home} initial={true} title="Home" />
-      <Scene key="login" component={About} title="About" />
-    </Scene>
-  </Router>
-);
+import App from './App';
 
 AppRegistry.registerComponent('ExpressReactHmrBoilerplate', () => App);
