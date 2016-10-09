97a3092ddb2e608ddb2ff7ebde172cfb3738518e
翁治平 <gocreating@gmail.com>
2016-10-10 04:48:33 +0800

    Support loading configs from env if on travis server

--- configs/project/server.js
+++ configs/project/server.js
@@ -1,8 +1,18 @@
-module.exports = {
-  jwt: {
-    secret: '4eO5viHe23',
-    expiresIn: 60 * 60 * 24 * 3, // in seconds
-  },
-  mongo: require('./mongo/credential'),
-  firebase: require('./firebase/credential.json'),
-};
+if (process.env.TRAVIS) {
+  if (!process.env.PROJECT_SERVER_CONFIGS) {
+    throw new Error(
+      'Environment variable `PROJECT_SERVER_CONFIGS` is not set. ' +
+      'Please dump by running `gulp dumpConfigs`'
+    );
+  }
+  module.exports = JSON.parse(process.env.PROJECT_SERVER_CONFIGS);
+} else {
+  module.exports = {
+    jwt: {
+      secret: '4eO5viHe23',
+      expiresIn: 60 * 60 * 24 * 3, // in seconds
+    },
+    mongo: require('./mongo/credential'),
+    firebase: require('./firebase/credential.json'),
+  };
+}
