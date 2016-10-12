1c09229ab2abcfed31f5909e375253e92e8e8625
翁治平 <gocreating@gmail.com>
2016-10-13 01:05:10 +0800

    Update error message

--- configs/project/server.js
+++ configs/project/server.js
@@ -2,7 +2,7 @@ if (process.env.TRAVIS) {
   if (!process.env.PROJECT_SERVER_CONFIGS) {
     throw new Error(
       'Environment variable `PROJECT_SERVER_CONFIGS` is not set. ' +
-      'Please dump by running `gulp dumpConfigs`'
+      'Please dump it by running `gulp dumpConfigs`'
     );
   }
   module.exports = JSON.parse(process.env.PROJECT_SERVER_CONFIGS);
