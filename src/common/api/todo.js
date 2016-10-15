4de116ae4d92f07bfb5acdde6a5099e76d3ff475
翁治平 <gocreating@gmail.com>
2016-10-15 15:12:31 +0800

    Add API Todo#Update

--- src/common/api/todo.js
+++ src/common/api/todo.js
@@ -6,5 +6,6 @@ export default (apiEngine) => ({
   // }),
   list: () => apiEngine.get('/api/todos'),
   create: (todo) => apiEngine.post('/api/todos', { data: todo }),
+  update: (id, todo) => apiEngine.put(`/api/todos/${id}`, { data: todo }),
   remove: (id) => apiEngine.del(`/api/todos/${id}`),
 });
