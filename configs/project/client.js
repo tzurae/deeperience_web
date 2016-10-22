6c5f43884f9dbe88e04a1ada74f3a8d4118a053a
翁治平 <gocreating@gmail.com>
2016-10-23 00:06:22 +0800

    Add GA tracking id configs

--- configs/project/client.js
+++ configs/project/client.js
@@ -1,3 +1,11 @@
 module.exports = {
   firebase: require('./firebase/client'),
+  GA: {
+    development: {
+      trackingID: 'UA-86112397-2',
+    },
+    production: {
+      trackingID: 'UA-86112397-1',
+    },
+  },
 };
