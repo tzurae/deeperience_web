63c1fd20b97637a369e59396043e3cc1de70f59e
翁治平 <gocreating@gmail.com>
2016-10-23 20:24:37 +0800

    Add passport facebook strategy

--- src/server/middlewares/passport.js
+++ src/server/middlewares/passport.js
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy } from 'passport-jwt';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
 import configs from '../../../configs/project/server';
 import User from '../models/User';
 
@@ -20,6 +21,41 @@ passport.use(new JwtStrategy({
     } else {
       done(null, false);
     }
+  });
+}));
+
+function findOrCreateUser(schemaProfileKey, email, cb) {
+  if (!email) {
+    return cb(new Error('Email is required'));
+  }
+  User.findOne({ 'email.value': email }, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    if (!user) {
+      user = new User();
+    }
+    if (!user.social.profile[schemaProfileKey]) {
+      user.social.profile[schemaProfileKey] = {};
+    }
+    return cb(null, user);
+  });
+}
+
+passport.use(new FacebookStrategy({
+  ...configs.passportStrategy.facebook.default,
+  ...configs.passportStrategy.facebook[process.env.NODE_ENV],
+}, (req, accessToken, refreshToken, profile, done) => {
+  findOrCreateUser('facebook', profile._json.email, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    // map `facebook-specific` profile fields to our custom profile fields
+    user.social.profile.facebook = profile._json;
+    user.email.value = user.email.value || profile._json.email;
+    user.name = user.name || profile._json.name;
+    user.avatarURL = user.avatarURL || profile._json.picture.data.url;
+    done(null, user);
   });
 }));
 
