9c7a48a5ec04b622bc6a54cc6441fd2d9f3dafbb
翁治平 <gocreating@gmail.com>
2016-10-23 17:02:17 +0800

    Add configs

--- configs/project/server.js
+++ configs/project/server.js
@@ -14,5 +14,8 @@ if (process.env.TRAVIS) {
     },
     mongo: require('./mongo/credential'),
     firebase: require('./firebase/credential.json'),
+    passportStrategy: {
+      facebook: require('./passportStrategy/facebook/credential'),
+    },
   };
 }
