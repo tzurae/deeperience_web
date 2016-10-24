8dcb7d62285766d6f83966c71bfe6443b502af75
翁治平 <gocreating@gmail.com>
2016-10-24 14:50:16 +0800

    Route to controlled next path after social login

--- src/server/controllers/socialAuth.js
+++ src/server/controllers/socialAuth.js
@@ -1,10 +1,18 @@
 import passport from 'passport';
 
 export default {
-  initFacebook: passport.authenticate('facebook', {
-    scope: ['public_profile', 'email'],
-  }),
-  initLinkedin: passport.authenticate('linkedin', {
-    state: Math.random(),
-  }),
+  initFacebook: (req, res, next) => (
+    passport.authenticate('facebook', {
+      scope: ['public_profile', 'email'],
+      state: JSON.stringify({ next: req.query.next }),
+    })(req, res, next)
+  ),
+  initLinkedin: (req, res, next) => (
+    passport.authenticate('linkedin', {
+      state: JSON.stringify({
+        next: req.query.next,
+        random: Math.random(),
+      }),
+    })(req, res, next)
+  ),
 };
