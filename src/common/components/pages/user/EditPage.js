a896caa58402425d4d6da0a2ac5d71cb83011ed2
翁治平 <gocreating@gmail.com>
2016-10-29 06:01:26 +0800

    Add AvatarForm into User#EditPage

--- src/common/components/pages/user/EditPage.js
+++ src/common/components/pages/user/EditPage.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
 import PageLayout from '../../layouts/PageLayout';
 import EditForm from '../../forms/user/EditForm';
+import AvatarForm from '../../forms/user/AvatarForm';
 import ChangePasswordForm from '../../forms/user/ChangePasswordForm';
 
 let EditPage = () => {
@@ -23,6 +24,10 @@ let EditPage = () => {
         <Col md={6}>
           <PageHeader>Edit Profile</PageHeader>
           <EditForm />
+        </Col>
+        <Col md={6}>
+          <PageHeader>Upload Avatar</PageHeader>
+          <AvatarForm />
         </Col>
         <Col md={6}>
           <PageHeader>Change Password</PageHeader>
