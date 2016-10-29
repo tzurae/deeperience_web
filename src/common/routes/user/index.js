e905e3b58b3c60cdf7068af12834ac2703c7a468
翁治平 <gocreating@gmail.com>
2016-10-29 18:20:38 +0800

    Add User#ForgetPasswordPage

--- src/common/routes/user/index.js
+++ src/common/routes/user/index.js
@@ -7,6 +7,7 @@ export default (store) => ({
         require('./verifyEmail').default(store),
         require('./login').default(store),
         require('./edit').default(store),
+        require('./forgetPassword').default(store),
         require('./logout').default(store),
         require('./me').default(store),
       ]);
