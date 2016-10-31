47de1152d717a5472cc8ae8132de4c396b88e7ed
翁治平 <gocreating@gmail.com>
2016-11-01 00:08:36 +0800

    Extract social buttons into component SocialAuthButtonList

--- src/common/components/pages/user/LoginPage.js
+++ src/common/components/pages/user/LoginPage.js
@@ -1,58 +1,34 @@
 import React from 'react';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
 import Alert from 'react-bootstrap/lib/Alert';
-import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import PageLayout from '../../layouts/PageLayout';
-import Head from '../../widgets/Head';
 import LoginForm from '../../forms/user/LoginForm';
+import SocialAuthButtonList from '../../utils/SocialAuthButtonList';
 
-let LoginPage = ({ location }) => {
-  let { next } = location.query;
-  let search = next ? '?next=' + next : '';
-
-  return (
-    <PageLayout hasGrid={false}>
-      <Head
-        links={[
-          'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.0.0/bootstrap-social.min.css',
-        ]}
-      />
-      <Grid>
-        <PageHeader>Login</PageHeader>
-        <Row>
-          <Col md={12}>
-            {next && (
-              <Alert bsStyle="warning">
-                <strong>Authentication Required</strong>
-                {' '}Please login first.
-              </Alert>
-            )}
-          </Col>
-        </Row>
-        <Row>
-          <Col md={9}>
-            <LoginForm />
-          </Col>
-          <Col md={3}>
-            <a
-              href={`/auth/facebook${search}`}
-              className="btn btn-block btn-social btn-facebook"
-            >
-              <span className="fa fa-facebook"></span>Login with Facebook
-            </a>
-            <a
-              href={`/auth/linkedin${search}`}
-              className="btn btn-block btn-social btn-linkedin"
-            >
-              <span className="fa fa-linkedin"></span>Login with LinkedIn
-            </a>
-          </Col>
-        </Row>
-      </Grid>
-    </PageLayout>
-  );
-};
+let LoginPage = ({ location }) => (
+  <PageLayout>
+    <PageHeader>Login</PageHeader>
+    <Row>
+      <Col md={12}>
+        {location.query.next && (
+          <Alert bsStyle="warning">
+            <strong>Authentication Required</strong>
+            {' '}Please login first.
+          </Alert>
+        )}
+      </Col>
+    </Row>
+    <Row>
+      <Col md={9}>
+        <LoginForm />
+      </Col>
+      <Col md={3}>
+        <SocialAuthButtonList />
+      </Col>
+    </Row>
+  </PageLayout>
+);
 
 export default LoginPage;
--- src/common/components/pages/user/RegisterPage.js
+++ src/common/components/pages/user/RegisterPage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import PageLayout from '../../layouts/PageLayout';
 import RegisterForm from '../../forms/user/RegisterForm';
+import SocialAuthButtonList from '../../utils/SocialAuthButtonList';
 
 const RegisterPage = (props) => (
   <PageLayout>
     <PageHeader>Register</PageHeader>
-    <RegisterForm />
+    <Row>
+      <Col md={9}>
+        <RegisterForm />
+      </Col>
+      <Col md={3}>
+        <SocialAuthButtonList />
+      </Col>
+    </Row>
   </PageLayout>
 );
 
