486565196fb2a4102edf0388dd495840277c830c
Larry <pudding850806@gmail.com>
2016-11-11 16:12:47 +0800

    Finish login page

--- src/common/components/pages/user/LoginPage.js
+++ src/common/components/pages/user/LoginPage.js
@@ -6,27 +6,49 @@ import Col from 'react-bootstrap/lib/Col'
 import PageLayout from '../../layouts/PageLayout'
 import LoginForm from '../../forms/user/LoginForm'
 import SocialAuthButtonList from '../../utils/SocialAuthButtonList'
+import BackGround from '../../utils/BackGround'
 
 const LoginPage = ({ location }) => (
   <PageLayout>
+    <BackGround src="/img/river_dark.jpg" />
     <PageHeader>Login</PageHeader>
+    {/*
+      <Row>
+        <Col md={12}>
+          {location.query.next && (
+            <Alert bsStyle="warning">
+              <strong>Authentication Required</strong>
+              {' '}Please login first.
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    */}
     <Row>
-      <Col md={12}>
-        {location.query.next && (
-          <Alert bsStyle="warning">
-            <strong>Authentication Required</strong>
-            {' '}Please login first.
-          </Alert>
-        )}
+      <Col md={4}>
+      </Col>
+      <Col md={4}>
+        <div style={{textAlign: 'center', color: 'white', marginBottom: '40px', marginTop: '-90px'}}>
+          <div style={{fontSize: '38px', marginBottom: '10px'}}> Deeperience </div>
+          <div style={{fontSize: '18px'}}> 完美客製化旅程，旅程規劃零負擔 </div>
+        </div>
+      </Col>
+      <Col md={4}>
       </Col>
     </Row>
     <Row>
-      <Col md={9}>
+      <Col md={4}>
+      </Col>
+      <Col md={4}>
         <LoginForm />
       </Col>
-      <Col md={3}>
-        <SocialAuthButtonList />
+      <Col md={4}>
       </Col>
+      {/*
+        <Col md={3}>
+          <SocialAuthButtonList />
+        </Col>
+      */}
     </Row>
   </PageLayout>
 )
