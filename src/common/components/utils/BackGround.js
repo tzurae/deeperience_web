d270b590b43f834a484fdc21af1e37f043c7e21e
Larry <pudding850806@gmail.com>
2016-11-14 09:29:18 +0800

    Modify background style

--- src/common/components/utils/BackGround.js
+++ src/common/components/utils/BackGround.js
@@ -5,18 +5,23 @@ const style = {
   position: 'absolute',
   top: '0px',
   left: '0px',
+  zIndex: '-1',
+  height: '100%',
 }
 
-const BackGround = ({ src, color }) => {
+const BackGround = ({ src, color, children }) => {
   if(src){
     return <img src={src} style={style} />
   } else {
     color = color || 'white'
     const extraStyle = {
       backgroundColor: color,
-      height: '100%',
     }
-    return <div style={{...style, ...extraStyle}}></div>
+    return (
+      <div style={{...style, ...extraStyle}}>
+        {children ? children : null}
+      </div>
+    ) 
   }
 }
 
