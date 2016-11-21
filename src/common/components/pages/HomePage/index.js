74804f163b8cbc5000556987b32fdc37c449599c
deeperience <gofriending@gmail.com>
2016-11-21 15:56:19 +0800

    add url file loader by fixing the css:background-img problem

--- src/common/components/pages/HomePage/index.js
+++ src/common/components/pages/HomePage/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import PageLayout from '../../layouts/PageLayout'
-import PageHeader from 'react-bootstrap/lib/PageHeader'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
+import Button from 'react-bootstrap/lib/Button'
+import Text from '../../widgets/Text'
 import s from './styles.scss'
 
 const HomePage = (props) => (
-  <PageLayout>
-    <PageHeader>Express-React-HMR-Boilerplate</PageHeader>
-    <p className={s.redBorder}>
-      This is the demo site for project{' '}
-      <a href="https://github.com/gocreating/express-react-hmr-boilerplate">
-        express-react-hmr-boilerplate
-      </a>
-    </p>
+  <PageLayout hasGrid={null}>
+    <Row>
+        <Jumbotron className={s.jumbotron}>
+          <Text id="company.name" />
+          <Text id="company.slogan"/>
+          <Button bsStyle="warning">
+            <Text id="produce.trip"/>
+          </Button>
+        </Jumbotron>
+    </Row>
   </PageLayout>
 )
 
