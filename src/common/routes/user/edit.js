6268f88fe0ec251a138ba3155650b8ffcf9064db
deeperience <gofriending@gmail.com>
2017-01-06 22:13:49 +0800

    remove resetPassword and edit of routes

--- src/common/routes/user/edit.js
+++
@@ -1,9 +0,0 @@
-export default (store) => ({
-  path: 'me/edit',
-  getComponent(nextState, cb) {
-    require.ensure([], (require) => {
-      cb(null, require('../../container/user/EditPage').default)
-    })
-  },
-  onEnter: require('../../utils/authRequired').default(store),
-})
--- src/common/routes/user/index.js
+++ src/common/routes/user/index.js
@@ -6,9 +6,7 @@ export default (store) => ({
         require('./register').default(store),
         require('./verifyEmail').default(store),
         require('./login').default(store),
-        require('./edit').default(store),
         require('./forgetPassword').default(store),
-        require('./resetPassword').default(store),
         require('./logout').default(store),
         require('./me').default(store),
       ])
